b044e8f692a108649eb8c4204571262dcf4150c5
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2019-11-11 11:52:46 -0800

    Updated A couple of things
    - Made a working scheduler engine (main.py)
    - Fixed the settings parser (main.py)
    - Updated requirements for the scheduler

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 #--------------------------------------------------#
 # Credit to Libraries that I used:                 #
 #                                                  #
-# WxPython: www.wxpython.org                       #
-# sys: Part of the standard python libraries       # 
+# WxPython(GUI programming): www.wxpython.org      #
+# sys(sys): Part of the standard python libraries  # 
 #      https://docs.python.org/3/py-modindex.html  #
-# threading: Part of the standard python libraries #
+# threading(multithreading): Part of the standard  # 
+#    python libraries                              #
 #      https://docs.python.org/3/py-modindex.html  #
-# json: Part of the standard python libraries      #
+# json(reading json settings file): Part of the    # 
+#   standard python libraries                      #
 #      https://docs.python.org/3/py-modindex.html  #
 #--------------------------------------------------#
 
@@ -16,7 +18,7 @@ import threading
 import json
 
 #? List Order:
-#? Name, Class, Elective
+#? Name, Class
 
 #! GUI Code
 class mainBox(wx.Frame):
@@ -50,25 +52,105 @@ def engine(DataTable):
     #? Level 3 (Dictionary): The rooms in which the hours of the days of the week can take place
     #? Values for Level 3 (Dictionary): A certain subject in a specific room in an hour of a day of a week (An hour is a key, A subject is the value)
     Rooms = ["201", "202", "203", "204", "205", "206", "207"]
-    Classes = ["Math", "LA", "Grammar", "Science", "CS", "PE", "Spanish"]
+    Subjects = ["Math", "LA", "Grammar", "Science", "CS", "PE", "Spanish"]
+    GradeClass = ["7A", "7B", "7C", "7D"]
+    NumbTimesSubjectOccurWeek = {"Math":4, "LA":4, "Grammar":4, "Science":4, "CS":4, "PE":4, "Spanish":4}
+    NumbTimesOccuredWeek = {"7A":[0, 0, 0, 0, 0, 0, 0], "7B":[0, 0, 0, 0, 0, 0, 0], "7C":[0, 0, 0, 0, 0, 0, 0], "7D":[0, 0, 0, 0, 0, 0, 0]}
+    NumberOfSlotsPerDay = 7
     SchedulePoint = {}
     ScheduleTable = []
-    for i in range(len(Rooms)):
-        SchedulePoint[Rooms[i]] = Classes[i]
-    print(SchedulePoint)
-    for i in range(5):
+    ExitLoop = False
+    ExitLoop1 = False
+    # for i in range(len(Rooms)):
+    #     SchedulePoint[Rooms[i]] = Subjects[i]
+    for x in range(5): #? Setup the schedule list / dictionary thing. (Days)
         ScheduleRow = []
-        for i in range(7):
+        for y in range(NumberOfSlotsPerDay): #? Setup the schedule list / dictionary thing. (Rows in a Day)
             SchedulePoint = {}
+            for z in range(len(Rooms)):
+                SchedulePoint[Subjects[z]] = ["[EMPTY]", Rooms[z]]
             ScheduleRow.append(SchedulePoint)
         ScheduleTable.append(ScheduleRow)
+
+    #* DEPRECATED
+    # for x in range(5): #? Run the schedule table through scheduling logic 
+    #     for y in range(NumberOfSlotsPerDay):
+    #         for z in range(len(Rooms)):
+    #             try: #? Fill a certain time slot with a certain class that's taking a subject that's occuring during that time slot
+    #                 if NumbTimesOccuredWeek[GradeClass[z]][z] <= NumbTimesSubjectOccurWeek[z]: #? Checking if the No. times a subject has occured in a week has exceeded the maximum amount of times that subject can appear in a week.
+    #                     ScheduleTable[x][y][Subjects[z]][0] = GradeClass[z]
+    #                     NumbTimesOccuredWeek[GradeClass[z]][z] += 1
+    #             except Exception as err: #? Checking for index out of range to skip.
+    #                 print("Left empty!" + str(err))
+    #         Subjects.append(Subjects[0])
+    #         Subjects.pop(0)
+    #         for c in NumbTimesOccuredWeek:
+    #             NumbTimesOccuredWeek[c].append(NumbTimesOccuredWeek[c][0])
+    #             NumbTimesOccuredWeek[c].pop(0)
+    #         NumbTimesSubjectOccurWeek.append(NumbTimesSubjectOccurWeek[0])
+    #         NumbTimesSubjectOccurWeek.pop(0)
+    #* DEPRECATED
+    
+    for x in range(5): #? Loop for 5 the 5 days in the chart
+        n = 0 #? Set the current GradeClass indicator
+        for y in range(NumberOfSlotsPerDay): #? Loop for all 7 slots in a day
+            for z in range(len(Rooms)): #? Loop for all different subjects that could occur during one slot for the number of rooms there are.
+                ExitLoop = False
+                print(ScheduleTable[x][y])
+                try:
+                    if ScheduleTable[x][y][Subjects[z]][0] == "[EMPTY]": #? Check if the current slot is empty
+                        for i in range(NumberOfSlotsPerDay): #? Loop for check below
+                            if ScheduleTable[x][i][Subjects[z]][0] == GradeClass[n]: #? Check if subject has already been taken by a specific class in a certain other timeslot
+                                # print("x", x, "i", i, "GradeClass", GradeClass[n], "Subject", Subjects[z], ScheduleTable[x][i][Subjects[z]][0])
+                                # ScheduleTable[x][y][Subjects[z]][0] = "[EMPTY]"
+                                ExitLoop = True
+                                break
+                        if ExitLoop == True:
+                            continue
+                        for i in range(len(Rooms)): #? Loop for check below
+                            if ScheduleTable[x][y][Subjects[i]][0] == GradeClass[n]: #? Check if subject has already been filled in by specific class in current timeslot
+                                ExitLoop = True
+                                break
+                        if ExitLoop == False: #? If this slot and it's subject hasn't been filled before by a specific class then:
+                            if NumbTimesOccuredWeek[GradeClass[n]][z] < NumbTimesSubjectOccurWeek[Subjects[z]]: #? If this specific subject hasn't already appeared throughout the week specific # of times
+                                ExitLoop1 = False
+                                for i in range(len(NumbTimesOccuredWeek[GradeClass[n]])): #? Loop to check if there is a better slot
+                                    if NumbTimesOccuredWeek[GradeClass[n]][z] > NumbTimesOccuredWeek[GradeClass[n]][i]: #? Check if there is a better slot that this class can occupy (Normalize the final chart table result)
+                                        ScheduleTable[x][y][Subjects[i]][0] = GradeClass[n]
+                                        NumbTimesOccuredWeek[GradeClass[n]][i] += 1
+                                        ExitLoop1 = True
+                                        break
+                                if ExitLoop1 == False: #? Occupy the slot
+                                    ScheduleTable[x][y][Subjects[z]][0] = GradeClass[n]
+                                    NumbTimesOccuredWeek[GradeClass[n]][z] += 1
+
+                except Exception as err: #? Exception for index out of range
+                    if str(err) != "list index out of range":
+                        print(str(err))
+                if n < len(GradeClass):
+                    n += 1
+                else:
+                    n = 0
+    print(NumbTimesOccuredWeek)
+    
+    f = open("log.txt", "w") #? <DEBUG>
+    for row in ScheduleTable: 
+        print("--------------------------------------------------------------------------------------------------------")
+        f.write("--------------------------------------------------------------------------------------------------------\n")
+        for point in row:
+            print(point)
+            f.write(str(point) + "\n")
+    f.close() #? </DEBUG>
+    
 
 def settingsFunc():
     with open("settings.json") as settings_file:
         settings = json.load(settings_file)
-        for SettingNo in settings["settings"]:
-            for setting in SettingNo:
-                print(setting + ": " + str(SettingNo[setting]))
+        for SettingOpts in settings["settings"]:
+            for SettingNo in SettingOpts:
+                for setting in SettingOpts[SettingNo]:
+                    print(SettingNo + " - " + setting + ": " + str(SettingOpts[SettingNo][setting]))
+        console()
         # print("Input the new value for a setting #. ([Setting#]-[New Value])")
         # NewValue = input(" > ")
 
@@ -78,7 +160,7 @@ def helpFunc():
 
 def guiFunc():
     #TODO: Finish the damn GUI
-    print("Code for the GUI is there, but is currently too unstable to actually do anything, will continue after program is finished.")
+    print("Code for the GUI is there, but is currently too unstable to actually do anything, will continue to work on gui after program is finished.")
     # app = wx.App(False)
     # frame = mainBox("GUI")
     # frame.Show(True)
